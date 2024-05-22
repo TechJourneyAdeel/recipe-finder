e1e7cb9e165f4adece02efae811e4586d37c8f0b
TechJourneyAdeel <adeelilyas112003@gmail.com>
2024-05-23 00:36:37 +0500

    Style changes

--- assets/js/single-recipe.js
+++ assets/js/single-recipe.js
@@ -1,30 +1,34 @@
-let apiKey = "55c52881e4f083db749d5ba92931936f";
+window.addEventListener("DOMContentLoaded", () => {
+  let apiKey = "55c52881e4f083db749d5ba92931936f";
 
-fetch(
-  `https://api.edamam.com/search?q=biryani&app_id=b16a6ebc&app_key=${apiKey}&from=0&to=50`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    banner(data.hits);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+  fetch(
+    `https://api.edamam.com/search?q=biryani&app_id=b16a6ebc&app_key=${apiKey}&from=0&to=50`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      banner(data.hits);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-const banner = (data) => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const title = urlParams.get("title").trim().replace(/-/g, " ");
+  const banner = (data) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const title = urlParams.get("title").replace(/-/g, " ");
+    console.log(title);
 
-  data.forEach((item) => {
-    const label = item.recipe.label.toLocaleLowerCase();
+    data.forEach((item) => {
+      const label = item.recipe.label.toLocaleLowerCase();
 
-    if (label === title) {
-      const banner = document.querySelector(".single-banner-row");
-      banner.querySelector("h1").innerText = `${item.recipe.label}`;
-      banner
-        .querySelector(".single-image img")
-        .setAttribute("src", `${item.recipe.image}`);
-    }
-  });
-};
+      if (label === title) {
+        const banner = document.querySelector(".single-banner-row");
+        console.log(banner);
+        banner.querySelector("h1").innerText = `${item.recipe.label}`;
+        banner
+          .querySelector(".single-image img")
+          .setAttribute("src", `${item.recipe.image}`);
+      }
+    });
+  };
+});
